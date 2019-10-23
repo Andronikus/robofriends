24df00fae726ce81fe5d59e7064b467e69208248
Andronikus <tiagoafroque@gmail.com>
2019-10-23 22:49:44 +0100

    Updates

---
+++ precache-manifest.102725a816591445708aee6ece495104.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72337c278988b061686eb0fb7f7a04fa",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "310cc17ce84b328af93b",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6c9965a27c19ad9a14e5",
+    "url": "/robofriends/static/css/main.53195070.chunk.css"
+  },
+  {
+    "revision": "310cc17ce84b328af93b",
+    "url": "/robofriends/static/js/2.9877e751.chunk.js"
+  },
+  {
+    "revision": "6c9965a27c19ad9a14e5",
+    "url": "/robofriends/static/js/main.26960378.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  }
+]);
